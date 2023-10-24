67d572e9265fd7ec6132a33891075a52398462ac
Ryan Mwangi Kamau <kamrymwangi@gmail.com>
2023-10-24 12:36:33 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+  if (distance <= 400) {
+  console.log('This one is on me')
+} else if (distance > 2000){
+  return '30 dollars'
+
+  }
 }
 
 function ternaryCheckCity(){
